79a164acc7932604579293907e7e08a132a547e3
Scott Pierce <ddrscott@gmail.com>
2013-06-12 23:17:43 -0400

    add sensible settings

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-    app = express()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
   
 var cursors = require('./cursors.js');
 
@@ -9,7 +9,13 @@ server.listen(process.env.PORT, process.env.IP);
 
 app.configure(function(){
     // app.use(express.logger());
-    app.use(express.static(__dirname + '/public'));  
+    app.use(express.static(__dirname + '/public'));
+});
+
+io.configure(function() {
+    io.enable('browser client minification');
+    io.enable('browser client gzip');
+    io.set('transports', ['websocket', 'flashsocket']);
 });
 
 var clients = {};
@@ -26,10 +32,10 @@ io.sockets.on('connection', function (socket) {
     socket.emit('myself', myself);
   
     socket.on('move', function(data) {
-        //console.log("socket: " + socket.id + " recieved: " + data);
-        data['from'] = socket.id
+        //console.log("socket: " + socket.id + " received: " + data);
+        data.from = socket.id;
         socket.broadcast.emit('move', data);
-    })
+    });
     
     socket.on('disconnect', function () {
         delete clients[socket.id];
